04ec5f68f7d412c7767a65f01bbef44c3de85be4
celyes <celyes02@gmail.com>
2019-08-25 19:10:51 +0100

    add solution - 5.php

---
+++ solutions/5.php
@@ -0,0 +1,18 @@
+<?php 
+function take_a_walk(int $distance){
+
+    if($distance %2 == 1){ 
+        throw new Exception("take an even number of steps or no return home! <br>");
+    }
+    $steps = [];
+    $directions = ['n', 'e', 's', 'w'];
+    for($i = 1; $i <= $distance; $i+=2){
+        $index = array_rand($directions);
+        $steps[] = $directions[$index];
+        $steps[] = $directions[($index + 2) %4];
+    }
+    shuffle($steps);
+    $steps = implode(', ', $steps) . PHP_EOL;
+    return $steps;
+}
+echo "Steps to take : " . take_a_walk(58);
