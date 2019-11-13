51425693a87073ce5b80c12487dc506824227a24
celyes <celyes02@gmail.com>
2019-11-13 13:30:16 +0100

    add new solution: challenge 9

---
+++ solutions/9.php
@@ -0,0 +1,26 @@
+<?php 
+
+function formPhoneNumber(array $integers): string
+{
+
+    if(count($integers) != 10) {
+        throw new Exception('Please provide a valid array!');
+    }
+
+    $phone = "(";
+    foreach($integers as $index => $number){
+       if($number < 10 && $number >= 0){
+            $phone .= $number;
+            if($index == 2){
+                $phone .= ") ";
+            }elseif($index == 5){
+                $phone .= "-";
+            }
+       }else{
+            throw new Exception("Please provide a valid array!");
+       }
+    }
+    return $phone;
+
+}
+echo formPhoneNumber([4, 5, 5, 4, 5, 1, 7, 8, 9, 0]);
