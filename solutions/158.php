26d39887dcd2e557c58bda58c369984e9b8410a6
celyes <celyes02@gmail.com>
2020-01-12 15:58:48 +0100

    add solution for challenge #158

---
+++ solutions/158.php
@@ -0,0 +1,35 @@
+<?php
+// round the number to the closest value (255 or 0)
+function roundToClosest($color)
+{ 
+    if($color > 255){
+        return 255;
+    }elseif($color < 0){
+        return 0;
+    }
+    return $color;
+}
+
+function rgb($r, $g, $b)
+{
+
+    $r = roundToClosest($r);
+    $g = roundToClosest($g);
+    $b = roundToClosest($b);
+
+// convert to hexadecimal
+
+    $r = dechex($r);
+    $g = dechex($g);
+    $b = dechex($b);
+
+//add a "0" if the result's length is less than 2
+
+    if(strlen($r) < 2): $r = "0" . $r; endif;
+    if(strlen($g) < 2): $g = "0" . $g; endif;
+    if(strlen($b) < 2): $b = "0" . $b; endif;
+
+//return the color value in uppercase
+
+    return strtoupper("#" . $r . $g . $b);
+}
